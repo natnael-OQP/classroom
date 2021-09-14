099dfb19efc5a31924d107dd6dc243d621beba07
papa-react <makiya112545@gmail.com>
2021-09-14 13:23:03 -0700

    add redux styled-compnent material-ui/core and matrial-ui/icons and roboto font family

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// Global Style
+import Globalstyled from './globalstyle'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Globalstyled/>
+      <h1>Classroom</h1>
+    </>
   );
 }
 
---
+++ src/globalstyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+   *{
+        padding: 0;
+        margin: 0;
+        text-decoration: none;
+        outline: none;
+        border: none;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    }
+    html,body{
+        overflow-x: hidden;
+        background-color: #000;
+        color:#FFF;
+    }
+`;
+
+export default GlobalStyle;
